86d5779817024213e19324a6d174e485fa869408
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2022-12-20 16:22:22 +0300

    Added Retries on genserver

--- lib/sender/sender_server.ex
+++ lib/sender/sender_server.ex
@@ -2,10 +2,37 @@ defmodule Sender.SenderServer do
   use GenServer
 
   def init(initial_state) do
+    # Initate retries
+    Process.send_after(self(), :retry, 5000)
+
     IO.puts("Received arguments: #{inspect(initial_state)}")
     max_retries = Keyword.get(initial_state, :max_retries, 5)
     state = %{emails: [], max_retries: max_retries}
     {:ok, state}
+  end
+
+  def handle_info(:retry, state) do
+    {failed, done} =
+      Enum.split_with(state.emails, fn email ->
+        email.status == "failed" && email.retries < state.max_retries
+      end)
+
+      retried =
+      failed
+      |> Enum.map(fn failor ->
+        IO.puts("Retrying email #{failor.email}...")
+
+        new_status =
+          case Sender.send_email(failor.email) do
+            {:ok, "email_sent"} -> "sent"
+            :error -> "failed"
+          end
+
+        %{email: failor.email, status: new_status, retries: failor.retries + 1}
+      end)
+
+    Process.send_after(self(), :retry, 5000)
+    {:noreply, Map.put(state, :emails, retried ++ done)}
   end
 
   def handle_call(:get_state, _from, state) do
